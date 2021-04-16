cd91ab197d11365e997a8448a732e13ee68abf39
ksumesh431 <juuzou431@gmail.com>
2021-04-16 13:44:22 +0530

    final comments

--- flattenObject.js
+++ flattenObject.js
@@ -1,3 +1,6 @@
+//question:
+//You are given an object. Write a function to flatten it, where the term flatten is defined as to get all the keys of nested objects and bring them to same level.
+
 //Sample Input:
 // {
 //     newObj: {
@@ -17,35 +20,29 @@
 // Sample Output:
 // { 'newObj.obj2.obj5.one': 1, 'obj3.obj4.two': 2 }
 
-//bhai ye jo niche code he kuch samjh nhi aa rha function ka
 
-//question:
-//You are given an object. Write a function to flatten it, where the term flatten is defined as to get all the keys of nested objects and bring them to same level.
 
 
 function flattenObject(ob) {
-    var toReturn = {};
+    let toReturn = {};
 
-    for (var i in ob) {
-        if (ob.hasOwnProperty(i) == null) {
-            continue;               //go to next iteration if there is no property inside(if the current iteration element is not present in object)or null)
+    for (let i in ob) {   //1st iteration for 'newObj' and 2nd iteration for 'obj3'
+        if (ob.hasOwnProperty(i) == null) {  //base case
+            return {};             //return empty object if there is no property inside(if the current iteration element is not present in object)or null)
         }
 
         if ((typeof ob[i]) == 'object' && ob[i] !== null) { //if the element in the iteration is object and is not null.. then enter loop
 
             var flatObject = flattenObject(ob[i]);
-            for (var x in flatObject) {
+            for (let x in flatObject) {
                 // console.log("Current i:" + i);
                 // console.log("Curent x:" + x+"\n");
-                if (flatObject.hasOwnProperty(x) == null) {   //if null then go to next iteration
-                    continue;
-                }
 
-                toReturn[i + '.' + x] = flatObject[x];   //after backtracking 1st time frm recursive call =>toReturn[obj5.one]=1
+                toReturn[i + '.' + x] = flatObject[x];   //after backtracking 1st time frm recursive call =>toReturn[obj5 . one]=1 ........ 2nd call toReturn[obj2 . obj5.one]=1 and so on
                 // console.log(toReturn)
             }
         } else {                                      //if the element in the iteration is not a object(or is a normal property)) or null
-            toReturn[i] = ob[i]; //returns { one: 1 } and during 2nd iteration while ob isobj3 it returns { two: 2 }
+            toReturn[i] = ob[i]; //returns { one: 1 }   and during 2nd iteration while ob is obj3 it returns { two: 2 }
         }
     }
     return toReturn;
